78d1ca85eeadde8f73fd9cf6d34ac9f774b4d37f
rubenfsgomes <ruben.gomes@uapgo.pt>
2023-04-04 14:34:08 +0100

    remove popup option

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 window.addEventListener("scroll", function() {
     // Check if user has reached the end of the page
-    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight-1) {
       // Show the pop-up
       var popup = document.getElementById("popup-container");
       popup.classList.add("show");
     }
+    if ((window.innerHeight + window.pageYOffset) < document.body.offsetHeight-300) {
+        // Show the pop-up
+        var popup = document.getElementById("popup-container");
+        popup.classList.remove("show");
+      }
   });
   
 const button = document.getElementById("close");
 
 button.addEventListener("click", function() {
     var popup = document.getElementById("popup-container");
-    popup.classList.remove("show");
+    popup.remove();
 });
